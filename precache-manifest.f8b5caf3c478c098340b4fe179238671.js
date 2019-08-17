dbed5701fc7f4948b2c8bcc228be076ac04509e9
SaberMyKing <liangxinleicn@gmail.com>
2019-08-17 12:35:59 +0800

    build react js

---
+++ precache-manifest.f8b5caf3c478c098340b4fe179238671.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e5bffb86a425c36b48f8f643178358",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b22416add60f62019b84",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1cd384bcf2ac044abd9c",
+    "url": "/static/js/2.5b8ca713.chunk.js"
+  },
+  {
+    "revision": "b22416add60f62019b84",
+    "url": "/static/js/main.c0f95743.chunk.js"
+  },
+  {
+    "revision": "9f4748b5e1ceb81ebd27",
+    "url": "/static/js/runtime~main.54dd181f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
